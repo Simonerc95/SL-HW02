b5e87a14812b5cddd8a47032c46befe1d50b0897
Simonerc95 <s.ercolino995@gmail.com>
2021-05-19 20:08:36 +0200

    training is working

--- point2.R
+++ point2.R
@@ -1,23 +1,31 @@
 load("data/amazon_review_clean.RData")
 library("MLmetrics")
 
-
-differentiation = function(X.square, X.y, beta){
-    return(X.square %*% beta - X.y)
+# changing labels of y vectors to book = TRUE, movie = FALSE
+y_tr = (y_tr == "book")
+y_te = (y_te == "book")
+differentiation = function(n, X.square, X.y, beta){
+    2/n * (X.square %*% beta - X.y)
 }
 
 
 X.square = t(X_tr) %*% X_tr
 X.y = t(X_tr) %*% y_tr
 
-beta = rnom(ncol(X_tr))
+beta = rnorm(ncol(X_tr))
 
+# learning rate
+alpha = 0.001
 
 loss = MSE(y_pred = X_tr %*% beta, y_true = y_tr)
-old.loss = loss + 1
-
-while (old_loss - loss > 0.0000001) {
+convergence = F
+loss.hist = c(loss)
+while (!convergence) {
+    beta = beta - alpha * differentiation(n=nrow(X_tr), X.square, X.y, beta)
     old.loss = loss
-    beta = beta - alpha * differentiation(square_matrix, lamble_matrix, beta)
     loss = MSE(y_pred = X_tr %*% beta, y_true = y_tr)
+    loss.hist <- cbind(loss.hist, c(loss))
+    if (old.loss - loss < 0.01){convergence = T}
 }
+
+plot(seq(40), loss.hist[390:429])
